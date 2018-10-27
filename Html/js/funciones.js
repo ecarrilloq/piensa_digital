59fcfbcbb1973792f583e406636cb49e3b50b8ac
Eduardo Carrillo <eacarrilloq@gmail.com>
2018-10-27 14:24:48 -0500

    Nuevos archivos

---
+++ Html/js/funciones.js
@@ -0,0 +1,42 @@
+console.log("Hola Mundo");
+var numero1 =10;
+var numero2 =10;
+var resultado = numero1+numero2;
+console.log(resultado);
+
+var cadena ="El resultado es: ";
+console.log(cadena+resultado);
+
+cadena ='Lorem ipsum \t dolor sit "amet" \n consectetur \"adipisicing\" elit.';
+console.log(cadena);
+
+var semana = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'];
+console.log(semana);
+console.log(semana[4]);
+
+semana[10]="AAA";
+console.log(semana);
+
+var cumple = true;
+console.log(!cumple);
+/*
+for(i=0;i<semana.length;i++){
+    console.log(semana[i]);
+}
+*/
+for (dia in semana){
+    if(dia%2==0){
+        console.log(dia);
+        console.log(semana[dia]);
+    }else{
+        console.log(dia);
+        console.log("Otro Valor");
+    }
+
+}
+
+/*
+semana.forEach(element => {
+    console.log(element);
+});
+*/
