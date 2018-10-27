b91bf2ffe028b9ea27c8d0cabb788430937e58d0
Eduardo Carrillo <eacarrilloq@gmail.com>
2018-10-27 17:02:48 -0500

    Nuevos archivos

--- Calculadora/js/funciones.js
+++ Calculadora/js/funciones.js
@@ -2,5 +2,10 @@ function calcular(val1,val2,operation){
     var inputRes=document.getElementById("resultado");
     var exp = val1 +" "+operation+" "+val2;
     console.log(exp);
-    inputRes.value= eval(exp);
+    if (operation=='/' && val2 ==0){
+        inputRes.value= "División por 0 no permitida"
+    }else{
+        inputRes.value= eval(exp);
+    }
+    
 }
